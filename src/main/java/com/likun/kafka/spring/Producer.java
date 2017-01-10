c5df5710fa845fd80b4fb42ff70f3c5392795e44
likun <1604398797@qq.com>
2017-01-10 12:22:30 +0800

    add spring..

---
+++ src/main/java/com/likun/kafka/spring/Producer.java
@@ -0,0 +1,14 @@
+package com.likun.kafka.spring;
+
+public class Producer {
+
+	public Producer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
